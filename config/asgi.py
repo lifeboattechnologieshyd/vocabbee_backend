5e430f7dd0c350270170e89ab1e1cd400c04dab0
charanlakkam331 <charanlakkam0309@gmail.com>
2026-05-30 20:11:05 +0530

    test api added

--- config/asgi.py
+++ config/asgi.py
@@ -13,6 +13,6 @@ from django.core.asgi import get_asgi_application
 
 os.environ.setdefault(
     "DJANGO_SETTINGS_MODULE",
-    "config.settings.production"
+    "config.settings.local"
 )
 application = get_asgi_application()
--- config/wsgi.py
+++ config/wsgi.py
@@ -13,6 +13,6 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault(
     "DJANGO_SETTINGS_MODULE",
-    "config.settings.production"
+    "config.settings.local"
 )
 application = get_wsgi_application()
