aeff632d52c9b720e1e023443c50da105789af45
ranjith <padidalaranjith@gmail.com>
2026-05-31 16:50:32 +0530

    few models added.

--- db/models/user.py
+++ db/models/user.py
@@ -4,7 +4,51 @@ from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
 from django.contrib.postgres.fields import ArrayField
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
+from crum import get_current_request
 
+
+class TimeAuditModel(models.Model):
+    """To path when the record was created and last modified"""
+
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Created At")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Last Modified At")
+
+    class Meta:
+        abstract = True
+
+
+class UserAuditModel(models.Model):
+    """To path when the record was created and last modified"""
+
+    created_by = models.CharField(
+        max_length=255,
+        verbose_name="Created By",
+        null=True,
+    )
+    updated_by = models.CharField(
+        max_length=255,
+        verbose_name="Updated By",
+        null=True,
+    )
+
+    class Meta:
+        abstract = True
+
+
+class AuditModel(TimeAuditModel, UserAuditModel):
+    """To path when the record was created and last modified"""
+
+    class Meta:
+        abstract = True
+
+    def save(self, *args, **kwargs):
+        request = get_current_request()
+        if request and hasattr(request, "user"):
+            if not self.created_by:
+                self.created_by = str(request.user.id)
+            self.updated_by = str(request.user.id)
+
+        super().save(*args, **kwargs)
 
 
 class CustomUserManager(BaseUserManager):
@@ -25,9 +69,18 @@ class UserMaster(AbstractBaseUser):
     mobile = models.BigIntegerField(
         validators=[MinValueValidator(1000000000), MaxValueValidator(9999999999)], unique=True
     )
+    full_name = models.CharField(
+        max_length=100,
+        blank=True,
+        null=True
+    )
+    profile_image = models.CharField(
+        max_length=500,
+        blank=True,
+        null=True
+    )
     is_mobile_verified = models.BooleanField(default=False)
     user_role = ArrayField(models.CharField(max_length=50, ), blank=True, null=True)
-    email = models.EmailField(max_length=100, null=True)
     is_active = models.BooleanField(default=True)
     created_by = models.CharField(
         max_length=255,
@@ -40,18 +93,77 @@ class UserMaster(AbstractBaseUser):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-
-
     objects = CustomUserManager()
 
     USERNAME_FIELD = "mobile"
     REQUIRED_FIELDS = []
+    class Meta:
+        db_table = "user_master"
 
     def __str__(self):
         return str(self.mobile)
 
-
-
+class OTPs(AuditModel):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    mobile_number = models.CharField(max_length=15)
+    otp = models.CharField(max_length=6)
+    expires_at = models.DateTimeField()
+    is_active = models.BooleanField(default=True)
 
     class Meta:
-        db_table = "user_master"
+        db_table = "otp"
+
+class Grades(AuditModel):
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False
+    )
+    name = models.CharField(
+        max_length=50
+    )
+    sort_order = models.IntegerField(
+        default=1
+    )
+    is_active = models.BooleanField(
+        default=True
+    )
+    class Meta:
+        db_table = "grades"
+
+class Kids(AuditModel):
+
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False
+    )
+
+    parent = models.ForeignKey(
+        UserMaster,
+        on_delete=models.CASCADE,
+        related_name="kids"
+    )
+
+    grade = models.ForeignKey(
+        Grades,
+        on_delete=models.PROTECT,
+        related_name="kids"
+    )
+
+    name = models.CharField(
+        max_length=100
+    )
+
+    profile_image = models.CharField(
+        max_length=500,
+        blank=True,
+        null=True
+    )
+
+    is_active = models.BooleanField(
+        default=True
+    )
+
+    class Meta:
+        db_table = "kids"
