df189e2a8f1bcfb46e39896db3fe543ccc83f3f7
ranjith <padidalaranjith@gmail.com>
2026-06-03 10:24:41 +0530

    bo - login api's added. user = login api's added. words - model created. urls configured in config folder

--- backoffice/urls.py
+++ backoffice/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import SendOtp, VerifyOTP
 
 urlpatterns = [
-    path("backoffice/send-otp", SendOtp.as_view()),
-    path("backoffice/verify-otp", VerifyOTP.as_view()),
+    path("send-otp", SendOtp.as_view()),
+    path("verify-otp", VerifyOTP.as_view()),
 
 ]
--- user/urls.py
+++ user/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from .views import SendOtp,VerifyOTP
 
 urlpatterns = [
-    path("user/send-otp", SendOtp.as_view()),
-    path("user/verify-otp", VerifyOTP.as_view()),
-
+    path("send-otp", SendOtp.as_view()),
+    path("verify-otp", VerifyOTP.as_view()),
 ]
