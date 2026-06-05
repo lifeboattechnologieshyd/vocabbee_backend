0f49cdd7b32726596825c4ab81572e0fdd18ea59
ranjith <padidalaranjith@gmail.com>
2026-06-05 19:12:07 +0530

    kids apis added.

--- user/urls.py
+++ user/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import SendOtp, VerifyOTP, Profile, GradesList
+from .views import SendOtp, VerifyOTP, Profile, GradesList, AddKid
 
 urlpatterns = [
     path("send-otp", SendOtp.as_view()),
     path("verify-otp", VerifyOTP.as_view()),
     path("profile", Profile.as_view()),
     path("grades", GradesList.as_view()),
+    path("kids", AddKid.as_view()),
 ]
