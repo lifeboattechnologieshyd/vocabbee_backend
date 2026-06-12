e686bf4e5cb71f31afdaaa2746fc89d52efa3c23
charanlakkam331 <charanlakkam0309@gmail.com>
2026-06-12 12:19:06 +0530

    test gemini .py changes

--- shared/helper.py
+++ shared/helper.py
@@ -102,16 +102,16 @@ def import_word_from_schoolfirst(word_text):
         return None
 
 
-from google import genai
-import os
-
-client = genai.Client(
-    api_key=os.getenv("GEMINI_API_KEY")
-)
-
-response = client.models.generate_content(
-    model="gemini-2.5-flash",
-    contents="Explain the word 'abandon'."
-)
-
-print(response.text)
+# from google import genai
+# import os
+#
+# client = genai.Client(
+#     api_key=os.getenv("GEMINI_API_KEY")
+# )
+#
+# response = client.models.generate_content(
+#     model="gemini-2.5-flash",
+#     contents="Explain the word 'abandon'."
+# )
+#
+# print(response.text)
