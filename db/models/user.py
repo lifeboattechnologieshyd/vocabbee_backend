20392e0ccd387ec7f1c35d8b065788f26a7a82eb
ranjith <padidalaranjith@gmail.com>
2026-06-12 10:13:23 +0530

    practice stats n attempts

--- db/models/user.py
+++ db/models/user.py
@@ -84,7 +84,7 @@ class UserMaster(AbstractBaseUser):
         null=True
     )
     email = models.CharField(
-        max_length=20,
+        max_length=50,
         blank=True,
         null=True
     )
