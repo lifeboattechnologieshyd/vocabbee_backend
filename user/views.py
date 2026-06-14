b55df34272f68946da6d40a23e597e1805d145ff
charanlakkam331 <charanlakkam0309@gmail.com>
2026-06-14 12:11:53 +0530

    cron test

--- user/views.py
+++ user/views.py
@@ -495,3 +495,6 @@ class FileUploadView(APIView):
             return CustomResponse().errorResponse(
                 {"error": str(e)}, description="File upload failed", status=status.HTTP_400_BAD_REQUEST
             )
+
+
+
