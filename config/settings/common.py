a3e4ff0442c11adddafd72ce3f0c61a666254bf1
charanlakkam331 <charanlakkam0309@gmail.com>
2026-06-15 09:49:05 +0530

    cron test

--- config/settings/common.py
+++ config/settings/common.py
@@ -279,7 +279,7 @@ CRONJOBS = [
    # Every day at 12:05 AM schedule_daily_words
     ("*/1 * * * *","django.core.management.call_command",["schedule_daily_words"],),
 
-    ("*/1 * * * *", "django.core.management.call_command", ["test_cron"]),
+    ("*/1 * * * *", "django.core.management.call_command", ["test"]),
 
 
 ]
